22911092ac15c1bd4aa3a1093c5cfdc5571755fb
maryam shehu <62632033+Maryam-2525@users.noreply.github.com>
2023-10-12 17:14:39 -0700

    contact.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,65 @@
 const express = require('express')
+const contacts = require("../../models/contacts.json")
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json(contacts);
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const id = req.params.contactId;
+  const contact = contacts.find(contact => contact.id === id)
+
+  if(!contact){
+    res.status(404).json({massage: "contact not found"});
+  }else{
+    req.status(200).json({massage: "contact found"})
+  }
+
+  console.log(id);
+  res.json(contact);
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+
+  const addContact = {
+    id: Date.now().toString(),
+    name: name,
+    email: email,
+    phone: phone,
+  }
+  
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const id = req.params.contactId;
+  const contact = contacts.filter(contact => contact.id === id)
+
+  if(!contact){
+    res.status(404).json({massage: "massage not found"});
+  }else{
+    res.status(200).json({massage:"contact deleted"});
+  }
+
+  console.log(id);
+  res.json(contact);
+
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const id = req.params.contactId;
+  const contact = contacts.push(contact => contact.id === id)
+
+  if(!contact){
+    res.status(404).json({massage: "massage not found"});
+  }else{
+    res.status(200).json;
+  }
+
+  console.log(id);
+  res.json(contact);
+
 })
 
 module.exports = router
